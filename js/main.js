d8f2fbaef02aac9f5efb0186fc87f728b7475555
Andrey Bagaev <unbugster@gmail.com>
2020-11-10 19:20:03 +0300

    Вносит исправления по базовым и дополнительным

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
 var writeUsButton = document.querySelector(".contact-link");
 var writeUsPopup = document.querySelector(".modal-write-us");
-var mapPopup = document.querySelector(".modal-map");
 var mapButton = document.querySelector(".map-link");
+var mapPopup = document.querySelector(".modal-map");
+
+var form = writeUsPopup.querySelector("form");
+var loginLogin = writeUsPopup.querySelector("[name=name]");
+var loginEmail = writeUsPopup.querySelector("[name=email]");
 
 function handlePopupToggle(popup) {
   return function () {
     if (popup) {
-      popup.style.display = "block";
+      popup.classList.add("modal-show")
       var input = popup.querySelector("input");
       if (input) {
         input.focus();
       }
 
+      if (storage) {
+        loginLogin.value = storage;
+        loginPassword.focus();
+      } else {
+        loginLogin.focus();
+      }
+
       var closeButton = popup.querySelector(".modal-close");
       if (closeButton) {
         closeButton.addEventListener('click', function () {
-          popup.style.display = "none";
+          popup.classList.remove("modal-show");
+          popup.classList.remove("modal-error");
         })
       }
+
+      window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          if (popup.style.display = "block") {
+            evt.preventDefault();
+            popup.classList.add("modal-error");
+            popup.style.display = "none";
+          }
+        }
+      });
     }
   }
 }
@@ -28,3 +50,30 @@ if (writeUsButton) {
 if (mapButton) {
   mapButton.addEventListener('click', handlePopupToggle(mapPopup))
 }
+
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+
+form.addEventListener("submit" , function(evt){
+  if (!loginLogin.value || !loginEmail.value) {
+    evt.preventDefault();
+
+    writeUsPopup.classList.remove("modal-error");
+    writeUsPopup.offsetWidth = writeUsPopup.offsetWidth;
+    writeUsPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {localStorage.setItem("login", loginLogin.value);
+  }
+  }
+})
+
+
+
